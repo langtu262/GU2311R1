2e98cbd57c0ec088a39e62e752a056be14e21b09
langtu262 <trungkienit87@gmail.com>
2023-12-15 12:32:25 +0700

    20231215

---
+++ 20231008/ReNhanh/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ReNhanh
+{
+    class Program
+    { 
+        static void Main(string[] args) 
+        {
+            
+        }
+    
+    }
+
+}
---
+++ 20231206/TinhTyGia/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace tinhtygia
+{
+	class Program
+	{
+		private static void Main(string[] args)
+		{
+			float tygia;
+			float soluong;
+			Console.WriteLine("nhap ty gia: ");
+			tygia = float.Parse(Console.ReadLine());
+			Console.WriteLine("nhap so luong: ");
+			soluong = float.Parse(Console.ReadLine());
+			Console.WriteLine("so tien doi duoc= " + tygia * soluong);
+		}
+
+	}
+}
