3813a3c9f618127a7b6ebcecdcd0943b6032a74d
langtu262 <trungkienit87@gmail.com>
2023-12-15 14:23:17 +0700

    20231215

---
+++ 20231206/LoiChao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Loichao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vui long nhap ten ban: ");
+            String yourname = Console.ReadLine();
+            Console.WriteLine("hello: " + yourname);
+        }
+    
+    }
+
+}
