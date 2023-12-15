54f9e384daf55110a9764b34f7cec43807a52cee
langtu262 <trungkienit87@gmail.com>
2023-12-15 13:50:04 +0700

    20231215

--- 20231208/ReNhanh/Program.cs
+++ 20231208/ReNhanh/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks.Dataflow;
 namespace ReNhanh
 {
     class Program
-    { 
-        static void Main(string[] args) 
+    {
+        static void Main(string[] args)
         {
-            
+            int number = 0 ;
+            Console.WriteLine("nhap so can kiem tra: ");
+            number = int.Parse(Console.ReadLine());
+            if (number > 0)
+            {
+                Console.WriteLine("so ban nhap la so duong");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("So ban nhan la so am");
+            }
+            else 
+            {
+                Console.WriteLine("So ban bang 0");
+            }
         }
-    
     }
 
 }
