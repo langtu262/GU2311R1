3972d577ae3d5fbebb7b093272ea25225a1de4c6
langtu262 <trungkienit87@gmail.com>
2023-12-16 22:11:55 +0700

    20231216

---
+++ 20231208/Ptbbacnhat/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Ptbacnhat
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so a: ");
+            Double a=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("nhap so b: ");
+            Double b=Convert.ToDouble(Console.ReadLine());
+            if (a != 0)
+            {
+                Console.WriteLine("phuong trinh co nghiem là: " + -b / a);
+            }
+            else
+            {
+                if (b==0)
+                {
+                    Console.WriteLine("phuong trinh vo nghiem: ");
+                }
+                else
+                {
+                    Console.WriteLine("phuong trinh vo so nghiem");
+                }
+
+            }
+        }
+    }
+}
+    
