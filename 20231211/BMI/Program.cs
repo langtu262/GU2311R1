f81d6f75bb4a6d1d2c285855b1ceaf5447f54063
langtu262 <trungkienit87@gmail.com>
2023-12-16 22:58:47 +0700

    20231216

---
+++ 20231211/BMI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap chieu cao cua ban: ");
+            float ChieuCao = float.Parse(Console.ReadLine());
+            Console.Write("Nhap can nang cua ban: ");
+            float Cannang = float.Parse(Console.ReadLine());
+            Double BMI = Cannang / (ChieuCao * ChieuCao);
+            BMI= Math.Round(BMI,1);
+            Console.WriteLine("chi do BMI cua ban la: " + BMI);
+            if (BMI < 18)
+            {
+                Console.Write("Ban gay");
+            }
+            else if (BMI < 25)
+            { 
+                Console.Write("Ban binh thuong");
+            }
+            else if(BMI <30)
+            {
+                Console.Write("Ban thua can");
+            }
+            else if(BMI>30) 
+            {
+                Console.Write("Ban beo phi");
+            }
+            else
+            {
+                Console.Write("Ban nhap ko dung cu phap");
+            }
+        }
+    }
+}
