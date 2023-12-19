9a8667c8483f99578c8061185a0a07931539f3c0
langtu262 <trungkienit87@gmail.com>
2023-12-19 21:25:36 +0700

    20231219

---
+++ 20231213/Enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+//namespace Se
+enum Season
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
+}
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+        Season mySeason = Season.Spring;
+        switch (mySeason)
+        {
+            case Season.Spring: Console.WriteLine("Day la mua Xuan");
+                break;
+            case Season.Summer: Console.WriteLine("Day la mua He");
+                break;
+            case Season.Autumn: Console.WriteLine("Day la mua thu");
+                break;
+            case Season.Winter: Console.WriteLine("Day la mua Dong");
+                break;
+            default: Console.WriteLine("ban nhap sai");
+                break;
+        }
+        }
+    }
