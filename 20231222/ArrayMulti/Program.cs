1ecb4a30f81a3bf4cf81413a1c405053dea23ea7
langtu262 <trungkienit87@gmail.com>
2023-12-28 14:47:57 +0700

    Update Program.cs

--- 20231222/ArrayMulti/Program.cs
+++ 20231222/ArrayMulti/Program.cs
@@ -8,7 +8,7 @@ internal class Program
         Console.Write("nhap so cot cua mang: ");
         int y=int.Parse(Console.ReadLine());
         int[,] Array = new int[x,y];
-        for (int i = 0; i < x; i++) //nhap gia tri mang
+        /*for (int i = 0; i < x; i++) //nhap gia tri mang
         {
             Console.WriteLine("Nhap gia tri dong thu: " + (i + 1));
             for (int j = 0; j < y; j++)
@@ -26,7 +26,45 @@ internal class Program
                 Console.Write("\t");
             }
             Console.Write("\n");
+        }*/
+        for (int i = 0; i < Array.GetLength(0); i++) //nhap gia tri mang
+        {
+            Console.WriteLine("Nhap gia tri dong thu: " + (i + 1));
+            for (int j = 0; j < Array.GetLength(1); j++)
+            {
+                Console.Write($"nhap gia tri mang {i + 1},{j + 1}: ");
+                Array[i, j] = int.Parse(Console.ReadLine());
+            }
         }
+        Console.WriteLine("Gia tri cua mang la: ");
+        for (int i = 0; i < Array.GetLength(0); i++)
+        {
+            for (int j = 0; j < Array.GetLength(1); j++)
+            {
+                Console.Write(Array[i, j]);
+                Console.Write("\t");
+            }
+            Console.Write("\n");
+        }
+        int max = 0;
+        int a=0; 
+        int b=0;
+        for (int i = 0; i < Array.GetLength(0); i++)
+        {
+            for (int j = 0; j < Array.GetLength(1); j++)
+            {
+                if (Array[i, j] > max)
+                {
+                    max = Array[i, j];
+                    a = i;
+                    b = j;
+                }
+            }
+        }
+        Console.WriteLine($"gia tri lon nhat cua mang la: {max} - tai vi tri: {a+1},{b+1}  ");
+        
+
+
 
     }
 }
