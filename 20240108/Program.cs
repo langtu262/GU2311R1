02b2c44d620f1e9454d05f7b088c42886154371c
langtu262 <trungkienit87@gmail.com>
2024-01-15 10:49:51 +0700

    20240115

---
+++ 20240108/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+internal class Program
+{ 
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("nhap kich chiu rong hinh chu nhat");
+        double width = double.Parse(Console.ReadLine());
+        Console.WriteLine("nhap kich chiu cao hinh chu nhat");
+        double height = double.Parse(Console.ReadLine());
+        Rectangle
+    }
+}
