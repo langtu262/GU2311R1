10d66023179399e994c860e0b853f0aa4db772bd
langtu262 <trungkienit87@gmail.com>
2024-01-15 14:40:16 +0700

    20240115

---
+++ 20240108/Rectangle/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(" nhap chiu rong hinh chu nhat");
+        double width = double.Parse(Console.ReadLine());
+        Console.WriteLine(" nhap chiu cao hinh chu nhat");
+        double height = double.Parse(Console.ReadLine());
+        Rectangle myRectangle = new Rectangle(width, height);
+    }
+}
---
+++ 20240108/Rectangle/rectangle.cs
@@ -0,0 +1,13 @@
+public class Rectangle
+{
+    double width, height;
+    public Rectangle()
+    {
+
+    }
+    public Rectangle(double width, double height)
+    { 
+        this.width = width;
+        this.height = height;
+    }
+}
