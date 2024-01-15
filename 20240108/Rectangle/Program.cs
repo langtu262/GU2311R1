d76c45e5efbc34a56c28c2e928cd794ef77da918
langtu262 <trungkienit87@gmail.com>
2024-01-15 15:46:03 +0700

    20230115

--- 20240108/Rectangle/Program.cs
+++ 20240108/Rectangle/Program.cs
@@ -8,6 +8,9 @@ internal class Program
         double width = double.Parse(Console.ReadLine());
         Console.WriteLine(" nhap chiu cao hinh chu nhat");
         double height = double.Parse(Console.ReadLine());
-        Rectangle myRectangle = new Rectangle(width, height);
+        Rectangle myRectangle = new Rectangle(width, height,true); //gọi phương thức public Rectangle(double width, double height, bool change)
+        //Rectangle myRectangle = new Rectangle(width, height); //gọi phương thức public Rectangle(double width, double height)
+        myRectangle.PrintfArea(); // gọi phương thức từ class Rectangle
+        myRectangle.Perimetter(); // gọi phương thức từ class Rectangle
     }
 }
--- 20240108/Rectangle/rectangle.cs
+++ 20240108/Rectangle/rectangle.cs
@@ -1,13 +1,31 @@
-public class Rectangle
+﻿public class Rectangle
 {
-    double width, height;
-    public Rectangle()
+    double width, height;//được gán giá trị bởi this.width, và this.height
+    public Rectangle(double width, double height, bool change) // phương thức
     {
+        if (change)
+        { 
+        this.width = width / 2; 
+        this.width = height / 2;
 
+            Console.WriteLine("dai: "+width);
+            Console.WriteLine("cao: " + height);
+
+            Console.WriteLine(this.width);
+            Console.WriteLine(this.height);
+        }
     }
-    public Rectangle(double width, double height)
+    public Rectangle(double width, double height) //phương thức có đối là width và height
     { 
-        this.width = width;
-        this.height = height;
+        this.width = width; //giá trị witch lấy từ bên class Program
+        this.height = height; //giá trị height từ bên class Program
+    }
+    public void PrintfArea()//phương thức
+    {
+        Console.WriteLine("dien tich: " + this.width * this.height); 
+    }
+    public void Perimetter()// phương thức
+    {
+        Console.WriteLine("chu vi la: " + (this.width + this.height) * 2);
     }
 }
